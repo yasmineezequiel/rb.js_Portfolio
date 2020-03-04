8088b3d7f43994152c6c3934d6c4f4967e025398
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-04 11:39:55 +0100

    Rails generates of Skill model with title and percentage attribute and creation of skill schema table through migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_172314) do
+ActiveRecord::Schema.define(version: 2020_03_04_103121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_03_03_172314) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
