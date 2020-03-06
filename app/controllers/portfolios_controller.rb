69eb38722959ccf112492af4fac104c789bbb3f9
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-06 18:22:41 +0100

    Adding create action in portfolio controller rendering from the new form in view.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,4 +7,19 @@ class PortfoliosController < ApplicationController
     def new
         @portfolio_item = Portfolio.new
     end
+
+    def create
+        @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio.save
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+            format.json { render :show, status: :created, location: @portfolio }
+          else
+            format.html { render :new }
+            format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
 end
