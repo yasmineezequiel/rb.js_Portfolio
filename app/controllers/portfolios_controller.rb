7fb2beb5cc75730960a43f6e3d458a177000b192
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-06 18:06:16 +0100

    Adding new action in portfolios controller and creating new form view.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,8 @@ class PortfoliosController < ApplicationController
     def index
        @portfolio_items = Portfolio.all
     end
+
+    def new
+        @portfolio_item = Portfolio.new
+    end
 end
