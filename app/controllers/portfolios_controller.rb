87222a2dd7b23fe38d91d9ee349601c72a32b4a8
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-06 19:16:08 +0100

    Adding edit action and .find params in portfolio controller and adding a form in view edit.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,5 +21,9 @@ class PortfoliosController < ApplicationController
           end
         end
       end
+
+      def edit
+        @portfolio_item = Portfolio.find(params[:id])
+      end
     
 end
