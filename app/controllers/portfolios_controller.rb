ede552c603a5d5d6ebd4764777a999dcf7436a23
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-08 12:31:12 +0100

    Adding destroy action into portfolios controller and fixing indentation.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,46 +1,51 @@
 class PortfoliosController < ApplicationController
 
-    def index
-       @portfolio_items = Portfolio.all
-    end
+  def index
+      @portfolio_items = Portfolio.all
+  end
 
-    def new
-        @portfolio_item = Portfolio.new
-    end
+  def new
+      @portfolio_item = Portfolio.new
+  end
 
-    def create
-        @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    
-        respond_to do |format|
-          if @portfolio.save
-            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-            format.json { render :show, status: :created, location: @portfolio }
-          else
-            format.html { render :new }
-            format.json { render json: @portfolio.errors, status: :unprocessable_entity }
-          end
+  def create
+      @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  
+      respond_to do |format|
+        if @portfolio.save
+          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+          format.json { render :show, status: :created, location: @portfolio }
+        else
+          format.html { render :new }
+          format.json { render json: @portfolio.errors, status: :unprocessable_entity }
         end
       end
+    end
 
-      def edit
-        @portfolio_item = Portfolio.find(params[:id])
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+        if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+          format.json { render :show, status: :ok, location: @portfolio }
+        else
+          format.html { render :edit }
+          format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+        end
       end
+    end 
 
-      def update
-        @portfolio_item = Portfolio.find(params[:id])
-        respond_to do |format|
-            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-              format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-              format.json { render :show, status: :ok, location: @portfolio }
-            else
-              format.html { render :edit }
-              format.json { render json: @portfolio.errors, status: :unprocessable_entity }
-            end
-          end
-        end 
-
-      def show 
-        @portfolio_item = Portfolio.find(params[:id])
-      end     
-    
+  def show 
+    @portfolio_item = Portfolio.find(params[:id])
+  end  
+  
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    end
+  end
 end
