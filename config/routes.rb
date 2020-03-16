1823274a13b1a01ca54346b31f135f4cdae9e7e6
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-16 10:42:59 +0100

    Refactoring about and contact routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/about'
-  get 'pages/contact'
+  
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
   resources :blogs
 
   root to: 'pages#home'
