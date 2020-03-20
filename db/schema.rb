3192b40a3c99ab164fe45b100bd01ddedbbd9849
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-20 09:18:18 +0100

    Generating Topic model and creating migration table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_155056) do
+ActiveRecord::Schema.define(version: 2020_03_20_081720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 2020_03_16_155056) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
