765a14894e2420676bd1432bd9a067241b39f211
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-24 17:36:11 +0100

    Adding separated method for OpenStruct class and pasing into current_user function.

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,7 +6,12 @@ module CurrentUserConcern
     end
 
     def current_user
-        super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+        super || guest_user 
         # super is a method built in from devise for user and rails will check first if super is equal true and if not will test the right OpenStruct rails built in method.
+    end
+
+    def guest_user
+        OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+                # super is a method built in from devise for user and rails will check first if super is equal true and if not will test the right OpenStruct rails built in method.
     end
 end
