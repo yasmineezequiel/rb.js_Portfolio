1864d68956537d5d4d8816eb719feddb050019a8
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-24 17:29:39 +0100

    Adding new concern curren user file with current user def and sending to application controller for cleaner code and better practice.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,5 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include DeviseWhitelist
     include SetSource
-
-    def current_user
-        super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
-        # super is a method built in from devise for user and rails will check first if super is equal true and if it will return this function if not will test the right OpenStruct rails built in method.
-    end
+    include CurrentUserConcern
 end
---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,12 @@
+module CurrentUserConcern
+    extend ActiveSupport::Concern
+
+    included do
+        before_action :current_user
+    end
+
+    def current_user
+        super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+        # super is a method built in from devise for user and rails will check first if super is equal true and if not will test the right OpenStruct rails built in method.
+    end
+end
