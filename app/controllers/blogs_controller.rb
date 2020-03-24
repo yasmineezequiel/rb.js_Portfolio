dcc0ba063c0cbdcfe908a0d59adccc1f2c06e0bf
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-24 16:14:49 +0100

    Removing parameters =true example for learning other approaches.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -78,6 +78,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog)
+      params.require(:blog).permit(:title, :body)
     end
 end
