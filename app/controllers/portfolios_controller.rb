d5180abdf1e2bec27178cadc66331e5b246189e1
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-24 16:29:59 +0100

    Refactoring portfolios controller for better practice, such as eliminating all the attributes and adding it into a private method for cleaner code.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-      @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+      @portfolio = Portfolio.new(portfolio_params)
   
       respond_to do |format|
         if @portfolio.save
@@ -34,7 +34,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-        if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        if @portfolio_item.update(portfolio_params)
           format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
           format.json { render :show, status: :ok, location: @portfolio }
         else
@@ -55,4 +55,15 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was deleted.' }
     end
   end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :technologies_attributes: [:name]
+                                    )
+  end
+
 end
