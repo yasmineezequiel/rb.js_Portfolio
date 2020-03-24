41db27378b4b2a99b0360fa122e8c06c010cb9ea
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-24 18:48:40 +0100

    Inserting method for dynamic title.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
     include DeviseWhitelistConcern
     include SetSourceConcern
     include CurrentUserConcern
+
+    before_action :set_title
+
+    def set_title 
+        @page_title = "Y Portfolio | Scuba Coding"
+    end
 end
