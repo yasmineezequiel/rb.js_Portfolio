b2be13ddcdde6d137f241f9d2baf686515976c1e
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-25 16:38:30 +0100

    Helper method of log in and it's condition parsing into portfolio application and blog as login_helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-    def sample_helper
-        "<p> My Helper </p>".html_safe
+    def login_helper
+        if current_user.is_a?(User)
+            link_to "Logout", destroy_user_session_path, method: :delete
+        else 
+            link_to "Register", new_user_registration_path
+            link_to "Login", new_user_session_path 
+        end
     end
 end
