2127cdf0c8c3b712263d78c04f0b26812fad5d5b
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-25 17:08:14 +0100

    Appending register and login links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,9 @@ module ApplicationHelper
         if current_user.is_a?(User)
             link_to "Logout", destroy_user_session_path, method: :delete
         else 
-            link_to "Register", new_user_registration_path
-            link_to "Login", new_user_session_path 
+            (link_to "Register", new_user_registration_path) +
+            "<br>".html_safe +
+            (link_to "Login", new_user_session_path) 
         end
     end
 end
