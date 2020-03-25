6be82a35c459158b0aa0a905637fed4cac040194
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-25 17:46:24 +0100

    Refactoring source_helper function.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,8 @@ module ApplicationHelper
 
     def source_helper
         if session[:source]
-        "Thanks for visiting me from #{session[:source]}"
+            greeting = "Thanks for visiting me from #{session[:source]}"
+            content_tag(:p, greeting, class: "source-greeting")
         end 
     end
 end
