204068b4d1049ba271754f937d006e3081242973
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-04-02 16:08:15 +0200

    Implementing Heredoc ruby in order to sort repetitive nav bar code and implementing active pages rails built in method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,24 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end 
     end
+
+
+# heredoc approach for different navigation bars helper.
+    def nav_helper style, tag_type
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_me_path}" class="#{style} #{active? about_me_path}">About Me</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+
+NAV
+    
+    nav_links.html_safe
+
+    end
+
+    def active? path
+        "active" if current_page? path
+    end
 end
