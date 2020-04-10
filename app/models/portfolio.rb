d408f3070bb9f449650ba9a6cddec0ab1ea84810
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-04-10 12:06:15 +0200

    Refactoring btn placeholder for uploading img. Deleting unnecessary place holder for img.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,24 +1,16 @@
 class Portfolio < ApplicationRecord
-    has_many :technologies, dependent: :delete_all
-    accepts_nested_attributes_for :technologies,
-                                  reject_if: lambda { |attrs| attrs['name'].blank? }
-  
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies, dependent: :delete_all
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
-    mount_uploader :thumb_image, PortfolioUploader
-    mount_uploader :main_image, PortfolioUploader
-    
-    def self.react
-      where(subtitle: 'React')
-    end
+  validates_presence_of :title, :body
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
   
-    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-  
-    after_initialize :set_defaults
-  
-    def set_defaults
-      self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-      self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+  def self.react
+    where(subtitle: 'React')
   end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+end
