911cc4c3ac449e030ecfb5ee58120985ff8307bf
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-04-13 11:22:48 +0200

    Implementing new Comment class and associating with user and blogs table.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("@rails/actioncable")
+
+import 'bootstrap'
 
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
-import 'bootstrap'
+//= require cable
