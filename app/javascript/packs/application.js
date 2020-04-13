f812a07144eda5eba029c492bb10c12815316a74
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-04-13 10:31:49 +0200

    Integrating gritter for message alerts.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'bootstrap'
+import 'bootstrap';
+import 'gritter';
 import "cocoon";
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,4 +21,4 @@ import "cocoon";
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
+//= require gritter
