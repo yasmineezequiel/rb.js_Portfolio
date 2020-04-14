6d3da91bdf14a93f27a031604408c6fc990c0b5e
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-04-14 14:38:04 +0200

    Implementing topics show and index action Implementing topic partial link and rendering into index view. Adding basic styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'topics/index'
-  get 'topics/show'
+  resources :topics, only: [:index, :show]
+  
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'react-items', to: 'portfolios#react'
