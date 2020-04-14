49e795b8517adbf12811c7c3809560e207113ff9
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-04-14 13:46:04 +0200

    Implementing drop-down for blog topic, adding topic_id blogs params, association of blog belongs to topic.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,9 @@ class Blog < ApplicationRecord
     extend FriendlyId
     friendly_id :title, use: :slugged
 
-    validates_presence_of :title, :body
+    validates_presence_of :title, :body, :topic_id
+
+    belongs_to :topic
 
     def self.recent
         order("created_at DESC")
